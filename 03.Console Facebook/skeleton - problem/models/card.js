2acaed7867919a9fde3564c7118df756ee86854c
Minko Vasilev <chardow2307@gmail.com>
2017-02-13 16:32:33 +0200

    Not working

---
+++ 03.Console Facebook/skeleton - problem/models/card.js
@@ -0,0 +1,26 @@
+
+function Card(){
+
+this._id = Card._count++;
+this._name = '';
+this._column;
+	
+	this.setName = function(cardName){
+		this._name = cardName;
+	}
+	this.setColumn = function(columnIndex){
+		this._column=columnIndex;
+	}
+	
+	this.getColumn = function(){
+		return this._column;
+	}
+	this.getName = function(){
+		return this._name;
+	}
+	this.getId = function(){
+		return this._id;
+	}
+}
+
+Card._count=0;
